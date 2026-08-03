23afb541b7ff3cb0281689dfdea8a50aa37af6b9
Samuel Abreu <samuelabreudev@gmail.com>
2026-08-03 17:11:32 -0300

    feat: Adicionando imagens dos projetos

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -17,9 +17,9 @@ export const Header = () => {
 
     return (
         <header className="fixed top-0 left-0 w-full z-50 bg-(--bg)/80 backdrop-blur-sm border-b border-(--border)">
-            <Container className="flex items-center justify-between h-16 md:h-20">
+            <Container className="flex w-full items-center justify-between h-16 md:h-20">
                 {/* Logo */}
-                <a href="/" className="text-2xl font-bold text-(--text-h)">
+                <a href="/" className="text-2xl font-bold text-(--text-h) ">
                     SAM<span className="text-(--accent)">.DEV</span>
                 </a>
 
